15f90a294e5e3d04d79b01924c9785d1280d609f
Habib Y.H <nucleusae@gmail.com>
2017-06-03 15:52:00 +0400

    Update for Edge
    Updated webchat for edge, now it will show puzzle for flash and failure
    message is more visible too.

--- NBWChat/js/global.js
+++ NBWChat/js/global.js
@@ -1,3 +1,9 @@
-var flashconfig = { id: 'nbwchat', src: 'flashchat20.swf?update=20,34', version: [9, 115], width: '105', height: '16', allowScriptAccess: 'always', wmode: 'transparent' };
+var flashconfig = {
+    id: 'nbwchat', src: 'flashchat20.swf?update=20,35', version: [9, 115], width: '105', height: '16', allowScriptAccess: 'always', wmode: 'transparent',
+    onFail: function () {
+        document.getElementById("nbflashchat20").innerHTML = "<div id=\"flashfailnotice\">Flash plugin is not installed or it is disabled in your browser.</div>"
+            + flashembed.getHTML({ id: 'nbwchat', src: 'flashchat20.swf?update=20,35', wmode: 'opaque' });
+    }
+};
 var ircserverips = "167.114.203.99";
 var ircserverport = "6668";
